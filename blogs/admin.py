23a0b123fb800accd2c1b1ab820d33d9b42dae71
kevinccbsg <kevin2906@hotmail.com>
2016-09-24 10:51:32 +0200

    Register model to admin

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from blogs.models import Blog, Post, Category
 # Register your models here.
+admin.site.register(Blog)
+admin.site.register(Post)
+admin.site.register(Category)
