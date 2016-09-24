9198508708dc82eda4f716b5c28a4bd6cd2677c5
kevinccbsg <kevin2906@hotmail.com>
2016-09-24 21:47:59 +0200

    clean singup data and display it correctly

--- blogs/urls.py
+++ blogs/urls.py
@@ -3,8 +3,8 @@ from blogs.views import BlogsView, PostsView, PostDetailView
 
 urlpatterns = [
     url(r'^$', BlogsView.as_view(), name='blogs_list'),
-    url(r'^(?P<username>[a-z]\w+)$',
+    url(r'^(?P<username>[0-9A-z]\w+)$',
         PostsView.as_view(), name='posts_of_user_blog'),
-    url(r'^(?P<username>[a-z]\w+)/(?P<pk>[0-9]+)/$',
+    url(r'^(?P<username>[0-9A-z]\w+)/(?P<pk>[0-9]+)/$',
         PostDetailView.as_view(), name='post_user_detail'),
 ]
--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.forms import ModelForm
 
 
 class LoginForm(forms.Form):
@@ -9,8 +8,40 @@ class LoginForm(forms.Form):
     pwd = forms.CharField(label="Password", widget=forms.PasswordInput())
 
 
-class SignUpForm(ModelForm):
+class SignUpForm(forms.Form):
 
-    class Meta:
-        model = User
-        fields = ('username', 'password', 'first_name', 'last_name', 'email',)
+    required_css_class = 'required'
+    username = forms.RegexField(regex=r'^[0-9A-z]\w+$',
+                                max_length=30,
+                                label="Username",
+                                error_messages={'invalid': "This value may contain only letters and numbers characters."})
+    email = forms.EmailField(label="E-mail")
+    password1 = forms.CharField(widget=forms.PasswordInput,
+                                label="Password")
+    password2 = forms.CharField(widget=forms.PasswordInput,
+                                label="Password (again)")
+    fist_name = forms.CharField(max_length=30)
+    last_name = forms.CharField(max_length=30)
+
+    def clean_username(self):
+        username_existis = User.objects.filter(
+            username=self.cleaned_data('username', ''))
+        if username_existis.exists():
+            raise forms.VAlidationError('A user with that name already exists')
+        else:
+            return self.cleaned_data('username')
+
+    def clean_username(self):
+        email_existis = User.objects.filter(
+            email=self.cleaned_data('email', ''))
+        if username_existis.exists():
+            raise forms.VAlidationError('This email is already registered')
+        else:
+            return self.cleaned_data('email')
+
+    def clean(self):
+        if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
+            if self.cleaned_data['password1'] != self.cleaned_data['password2']:
+                raise forms.ValidationError(
+                    "The two password fields didn't match.")
+        return self.cleaned_data
