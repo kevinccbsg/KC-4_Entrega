89a1dc3e6c5f3e2a0e3c90abce94a5c9b5bccc0a
kevinccbsg <kevin2906@hotmail.com>
2016-09-24 11:17:25 +0200

    Urls of blogs created

---
+++ blogs/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from blogs.views import 
+
+urlpatterns = [
+    url(r'^$', , name='blogs_list'),
+    url(r'^(?P<username>[a-z]\w+)$', , name='posts_of_user_blog'),
+    url(r'^(?P<username>[a-z]\w+)/(?P<pk>[0-9]+)$', , name=''),
+]
